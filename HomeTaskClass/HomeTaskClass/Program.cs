11ac33a343747c8d49b81d3c41f97aeb0f49cdb3
PashaAllahverdiyev <129064604+PashaAllahverdiyev@users.noreply.github.com>
2024-04-01 09:57:48 +0400

    Add files via upload

---
+++ HomeTaskClass/HomeTaskClass/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace HomeTaskClass
+{
+   internal class Program
+    {
+        public static void Main(string[] arr)
+        {
+            //Person person = new Person("Pasha", "Allahverdiyev", 24);
+            //person.IsAdult();
+            //Car car = new Car("BMW","f10",2014,"Benzin","Ag");
+            //car.GetInfo();
+            //Car car = new Car("BMW","f10",2014,320,160);
+            //car.CheckSpeed();
+            //Motorcycle motorcycle = new Motorcycle("Suzuki","GSX-R125",2023,300,100);
+            //motorcycle.CheckSpeed();
+            //User user = new User
+            //{
+            //    Name = "Pasha",
+            //    Nikname = "asdf",
+            //    Password = "dsagfs45"
+            //};
+            //Console.WriteLine(user.IsPasswordValid());
+        }
+    }
+    //Task1
+    //public class Person
+    //{
+    //    public string Name;
+    //    public string Surname;
+    //    public byte Age;
+    //    public bool isAdult;
+    //    public Person(string name, string surname, byte age)
+    //    {
+    //        Name = name;
+    //        Surname = surname;
+    //        Age = age;
+
+    //    }
+    //    public void IsAdult()
+    //    {
+    //        if (Age >= 18)
+    //        {
+    //            isAdult = true;
+    //        }
+    //        else
+    //        {
+    //            isAdult = false;
+    //        }
+    //        Console.WriteLine(isAdult);
+    //    }
+    //}
+
+    //Task2
+    //public class Car
+    //{
+    //    public string Brend;
+    //    public string Model;
+    //    public int Year;
+    //    public string FuelType;
+    //    public string Color;
+    //    public Car(string brend, string model, int year, string fueltype, string color)
+    //    {
+    //        Brend = brend;
+    //        Model = model;
+    //        Year = year;
+    //        FuelType = fueltype;
+    //        Color = color;
+    //    }
+    //    public void GetInfo() 
+    //    {
+    //        Console.WriteLine($"Brend: {Brend}\n Model: {Model}\n Year: {Year}\n FuelType: {FuelType}\n Color: {Color}");
+    //    }
+    //}
+
+    //Task3
+
+
+    //Task4
+    //public class User
+    //{
+    //    public string Name { get; set; }
+    //    public string Nikname { get; set; }
+    //    public string Password { get; set; }
+        
+    //    public bool IsPasswordValid()
+    //    {
+    //       if(Password.Length<6 || !Password.Any(char.IsDigit)||Password.Contains(Name))
+    //        {
+    //            return false;
+    //        }
+    //       return true;
+    //    }
+    //}
+}
+ 
+
